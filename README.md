9c522276e1743b4f6449f0bc1a6a833192d21814
Jan <jan@kaios.ai>
2022-07-19 18:12:03 +0200

    added full functions

--- README.md
+++ README.md
@@ -14,3 +14,12 @@ https://github.com/nhatuan84/esp32-micro-sdcard
 Format sd card:
 
 open powershell as admin and: '''format /FS:FAT32 D:''' > change D: to your drive. 
+
+http://www.iotsharing.com/2017/05/how-to-use-arduino-esp32-to-store-data-to-sdcard.html
+
+C:\Users\jgm_6\AppData\Local\Arduino15\packages\esp32\hardware\esp32\2.0.4\variants\esp32s2\pins_arduino.h
+
+static const uint8_t SS    = 10;
+static const uint8_t MOSI  = 11;
+static const uint8_t MISO  = 13;
+static const uint8_t SCK   = 12;
--- src/src.ino
+++ src/src.ino
@@ -4,12 +4,60 @@
 #include ".\conf.h"
 #include ".\emotionchair_lib.h"
 
+#include <SPI.h>
+#include <SD.h>
 
+int count = 0;
 AsyncWebServer server(80);
-int state = 0;
+
+
+void printDirectory(File dir, int numTabs) {
+  while (true) {
+ 
+    File entry =  dir.openNextFile();
+    if (! entry) {
+      // no more files
+      break;
+    }
+    for (uint8_t i = 0; i < numTabs; i++) {
+      Serial.print('\t');
+    }
+    Serial.print(entry.name());
+    if (entry.isDirectory()) {
+      Serial.println("/");
+      printDirectory(entry, numTabs + 1);
+    } else {
+      // files have sizes, directories do not
+      Serial.print("\t\t");
+      Serial.print(entry.size(), DEC);
+      time_t lw = entry.getLastWrite();
+      struct tm * tmstruct = localtime(&lw);
+      Serial.printf("\tLAST WRITE: %d-%02d-%02d %02d:%02d:%02d\n", (tmstruct->tm_year) + 1900, (tmstruct->tm_mon) + 1, tmstruct->tm_mday, tmstruct->tm_hour, tmstruct->tm_min, tmstruct->tm_sec);
+    }
+    entry.close();
+  }
+}
+void writeFile(const char * path){
+    File file2 = SD.open(path, FILE_WRITE);
+    if(file2){
+    file2.println("test");
+    file2.flush();
+    file2.close();
+    }
+  }
 
 void setup() {
     Serial.begin(9600);
+      if (!SD.begin()) {
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("* is a card inserted?");
+    Serial.println("* is your wiring correct?");
+    Serial.println("* did you change the chipSelect pin to match your shield or module?");
+    while (1);
+  } else {
+    Serial.println("Wiring is correct and a card is present.");
+  }
+  
     Serial.print("Connecting to ");
     Serial.println(ssid);
     WiFi.begin(ssid, password);
@@ -24,10 +72,17 @@ void setup() {
     server.on("/status", HTTP_GET, [](AsyncWebServerRequest *request){
       request->send(200, "text/plain", String(state));
     });
+    server.on("/addfile", HTTP_GET, [](AsyncWebServerRequest *request){
+      writeFile("/hello123.txt");
+      request->send(200, "text/plain", String("Written file"));
+    });
     server.begin();
+    
+    
+    
+    File dir =  SD.open("/");
+    printDirectory(dir, 0);
 }
 
 void loop() {
-  state++;
-  delay(1000);
 }

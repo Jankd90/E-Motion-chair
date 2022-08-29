bb8b35b108eed3a8f7facb07a613914e89fa0c28
Frank <50660908+FranklySolutions@users.noreply.github.com>
2022-08-29 08:48:41 +0200

    changed src: added mDNS code

--- src/conf.h
+++ src/conf.h
@@ -1,12 +1,12 @@
-const char* ssid     = "AndroidAPE7EC";
-const char* password = "snun4082";
+//const char* ssid     = "AndroidAPE7EC";
+//const char* password = "snun4082";
 
 //const char* ssid     = "LaunchCafe";
 //const char* password = "Groningen";
 
-//const char* ssid     = "Verbindingsstraat 6";
-//const char* password = "NietVoorAlgemeenGebruik9745AH!";
+//const char* ssid     = "ziggo-ap-0441213";
+//const char* password = ".5*LN-AGDPbN,";
 
-//const char* ssid     = "Oneplus";
-//const char* password = "cashmeoutside";
+const char* ssid     = "Oneplus";
+const char* password = "cashmeoutside";
 String serverPath = "https://currentmillis.com/time/seconds-since-unix-epoch.php";
